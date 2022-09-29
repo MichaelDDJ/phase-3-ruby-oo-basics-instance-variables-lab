1bd23ae58d17f22f9643ca0b2f1e4b3f96b52de3
Michael Davis <jrmdd1496@gmail.com>
2022-09-29 15:33:31 -0500

    commit tree 502ac471c859097484313261a9420fa8de80a118

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
-
-end
+    # setter method
+    def name=(value)
+        @this_dogs_name = value
+    end
+  
+    # getter method
+    def name
+      # access the data from the @name instance variable and return it
+      @this_dogs_name
+    end
+  end
